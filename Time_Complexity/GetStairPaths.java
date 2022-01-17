a53ed2941cae7e5335b658a02e870c52081833a0
Sanjeev Sharma <sanjeevsharma1603@gmail.com>
2022-01-17 16:04:14 +0530

    GetStairPaths.java updated

--- Time_Complexity/GetStairPaths.java
+++ Time_Complexity/GetStairPaths.java
@@ -13,26 +13,27 @@ public class GetStairPaths {
 
     public static ArrayList<String> getStairPaths(int n) {
         if(n == 0){
-            ArrayList<String> bres = new ArrayList<>();
+            ArrayList<String> bres = new ArrayList<String>();
             bres.add("");
-            return;
+            return bres;
         }
         if(n<0){
-            ArrayList<String> bres = new ArrayList<>();
+            ArrayList<String> bres = new ArrayList<String>();
             return bres;
         }
         ArrayList<String> nm1 = getStairPaths(n-1);
         ArrayList<String> nm2 = getStairPaths(n-2);
         ArrayList<String> nm3 = getStairPaths(n-3);
+
         ArrayList<String> paths = new ArrayList<String>();
-        for(String path : paths1){
-            paths.add(1+path);
+        for(String path: nm1){
+            paths.add("1"+path);
         }
-        for(String path : paths2){
-            paths.add(2+path);
+        for(String path: nm2){
+            paths.add("2"+path);
         }
-        for(String path : paths3){
-            paths.add(3+path);
+        for(String path: nm3){
+            paths.add("3"+path);
         }
         return paths;
         
